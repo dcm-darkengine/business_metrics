41066621da2055a528830fe86220ee00d9b55e0b
dpodkhvatilin <dpodkhvatilin@naumen.ru>
2021-03-12 15:01:16 +0500

    preFinal

--- metrics.groovy
+++ metrics.groovy
@@ -1,50 +1,46 @@
 /*&silent*/
 import groovy.transform.Field
-import groovy.json.JsonBuilder
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
 import groovy.time.TimeCategory
-import groovy.time.TimeDuration
 
 @Field Date currentDate = new Date()
 @Field Date periodDate = use(TimeCategory) { currentDate - 1.minute }
 
 def serviceCallByStates() {
     String hqlServiceCallsByStates = '''
-SELECT count(*)
+SELECT sc.state, count(sc)
 FROM serviceCall sc
-WHERE sc.state = :stateCode
+GROUP BY sc.state
 '''
-    def serviceCallByStates = [:]
-
-    api.metainfo.getMetaClass('serviceCall').workflow.states.code.each { code ->
-        serviceCallByStates.put(code,
-                api.db.query(hqlServiceCallsByStates)
-                        .set('stateCode', code)
-                        .list()[0]
-                        .toString()
-        )
+    return api.db.query(hqlServiceCallsByStates)
+            .list().collect { row ->
+        return [
+                'state' : row[0],
+                'amount': row[1]
+        ]
     }
-
-    return serviceCallByStates
 }
 
 def periodComments() {
     String hqlPeriodComments = '''
-SELECT count(comm)
+SELECT comm.source
 FROM comment comm
 WHERE comm.creationDate between :periodDate and :currentDate
 AND comm.author is not null
-AND NOT EXISTS(
-SELECT id 
-FROM employee em
-WHERE em.lastName = 'Служебный'
-AND em.id = comm.author.id
-)
 '''
 
     def comments = api.db.query(hqlPeriodComments)
             .set('periodDate', periodDate)
             .set('currentDate', currentDate)
-            .list()[0]
+            .list()
+            .collect { row ->
+                return row.tokenize('$')[0]
+            }
+            .groupBy()
+            .collect { return ["class": it.key, "amount": it.value.size()] }
 }
 
 def periodServiceCalls() {
@@ -63,8 +59,7 @@ WHERE sc.creationDate between :periodDate and :currentDate
                 'author'          : row[1],
                 'requestDate'     : row[2],
                 'registrationDate': row[3],
-                //'registrationTime': TimeCategory.minus(row[2], row[3]),
-                'registrationTime': row[2] - row[3],
+                'registrationTime': row[3].getTime() - row[2].getTime(),
                 'wayAddressing'   : row[4]
         ]
     }
@@ -90,12 +85,20 @@ AND log.authorLogin is not 'naumen'
     }
 }
 
+def licenseUsage() {
+    api.employee.getLicensesUsage().collect {
+        return ["license": it.key,
+                "amount" : it.value]
+    }
+}
+
 def business_metrics = [
         'itsm365_comments_for_the_period'    : periodComments(),
         'itsm365_serviceCalls_for_the_period': periodServiceCalls(),
-        'itsm365_license_usage_per_user'     : api.employee.getLicensesUsage(),
+        'itsm365_license_usage_per_user'     : licenseUsage(),
         'itsm365_serviceCalls_per_states'    : serviceCallByStates(),
         'itsm365_admin_logs_for_the_period'  : periodAdminActions()
 ]
 
-return new JsonBuilder(business_metrics).toPrettyString()
+Gson gson = new GsonBuilder().setPrettyPrinting().create()
+return gson.toJson(business_metrics)
